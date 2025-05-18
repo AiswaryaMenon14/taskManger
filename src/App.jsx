7c8db42f39ec257e7ceee3f6e1ae5e1d726fe3dc
AiswaryaMenon14 <your-email@example.com>
2025-05-18 22:22:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import DisplayTodos from "./components/DisplayTodo";
+import Todos from "./components/Todo";
+
+import { motion } from "framer-motion";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <motion.h1
+        initial={{ y: -200 }}
+        animate={{ y: 0 }}
+        transition={{ type: "spring", duration: 0.5 }}
+        whileHover={{ scale: 1.1 }}
+      >
+        Todo App
+      </motion.h1>
+      <motion.div
+        initial={{ y: 1000 }}
+        animate={{ y: 0 }}
+        transition={{ type: "spring", duration: 1 }}
+      >
+        <Todos />
+        <DisplayTodos />
+      </motion.div>
+    </div>
+  );
+}
+
+export default App;
